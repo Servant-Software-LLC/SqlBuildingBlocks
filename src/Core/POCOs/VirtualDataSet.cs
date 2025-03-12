5871332d04a8bc697477aced120d620bf0fbc533
David Maltby <david.maltby@hotmail.com>
2025-03-11 20:35:37 -0500

    Add disposal to VitualDataSet

--- src/Core/POCOs/VirtualDataSet.cs
+++ src/Core/POCOs/VirtualDataSet.cs
@@ -2,7 +2,7 @@
 
 namespace SqlBuildingBlocks.POCOs;
 
-public class VirtualDataSet
+public class VirtualDataSet : IDisposable
 {
     private readonly IDictionary<string, VirtualDataTable> tables = new Dictionary<string, VirtualDataTable>(StringComparer.OrdinalIgnoreCase);
 
@@ -16,6 +16,33 @@ public class VirtualDataSet
     }
 
     public VirtualDataTableCollection Tables => new VirtualDataTableCollection(tables);
+
+    public void RemoveWithDisposal(string key)
+    {
+        if (tables.TryGetValue(key, out var table))
+        {
+            if (table is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+
+            Tables.Remove(key);
+        }
+    }
+
+    /// <summary>
+    /// Disposes all disposable virtual tables created by this VirtualDataSet.
+    /// </summary>
+    public void Dispose()
+    {
+        foreach (var table in tables)
+        {
+            if (table.Value is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
 
 }
 
